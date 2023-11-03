753da9d3f88872909dffa19ae62ed563d14f7488
Stephen Dawson <stephendawson@protonmail.com>
2023-11-03 10:52:56 +0000

    add account create and retrieve test

--- custom_account/models.py
+++ custom_account/models.py
@@ -7,5 +7,8 @@ class ParentUser(AbstractBaseUser):
     Django's User model as a base.
     """
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=60, unique=True)
+    first_name = models.CharField(max_length=40, blank=False, null=False)
+    last_name = models.CharField(max_length=40, blank=False, null=False)
 
     USERNAME_FIELD = 'email'
--- custom_account/tests.py
+++ custom_account/tests.py
@@ -1,16 +1,46 @@
 from django.test import TestCase
 from django.apps import apps
+from custom_account.models import ParentUser
 
 
 class AccountTests(TestCase):
     """
-    Test case to check if model is not none
+    Tests for creating, updating, and deleting
+    users in the custom_account app.
     """
     def test_check_create_user_view_exists(self):
+      """Test case to check if model is not none"""
       try:
-          UserModel = apps.get_model('account', 'User') 
+        UserModel = apps.get_model('custom_account', 'ParentUser')
       except LookupError as e:
-          self.fail(f"Failed to get User model. Error: {e}")
+        self.fail(f"Failed to get User model. Error: {e}")
+    
+    def test_create_and_retrieve_user(self):
+        """Test to create and get back a user from the database."""
+        try:
+            # Create a user with dummy data
+            user = ParentUser.objects.create(
+                email='test@test.com',
+                username='testuser',
+                first_name='Test',
+                last_name='User',
+                password='testpassword123',
+            )
+        except Exception as e:
+            self.fail(f"Failed to create user. Error: {e}")
+
+        try:
+            # Pull the user from the database if it exists
+            existing_user = ParentUser.objects.get(pk=user.pk)
+            
+            # Check the the data matches what was created
+            self.assertEqual(existing_user.email, 'test@test.com')
+            self.assertEqual(existing_user.username, 'testuser')
+            self.assertEqual(existing_user.first_name, 'Test')
+            self.assertEqual(existing_user.last_name, 'User')
+        except Exception as e:
+            self.fail(f"Could not get user from the database. Error: {e}")
+
 
 
         
