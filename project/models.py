376c4ae1ff1e7a2946f2b375db21c472fc4d9031
Stephen Dawson <stephendawson@protonmail.com>
2023-11-11 11:37:16 +0000

    remove unncessary import in user models.py

--- project/models.py
+++ project/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
-from custom_account.models import CustomUser
 
 
 class Project(models.Model):
