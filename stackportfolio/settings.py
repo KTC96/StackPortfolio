094fb03bf80c62abcd65cfa97691245cae8ed75f
Stephen Dawson <stephendawson@protonmail.com>
2023-11-11 11:34:50 +0000

    add render domain to allowed hosts

--- stackportfolio/settings.py
+++ stackportfolio/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stackportfolio-1340b88e89e7.herokuapp.com',
-                 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['stackportfolio-1340b88e89e7.herokuapp.com', 'stackportfolio.onrender.com',
+                 'localhost', '127.0.0.1', 'stackportfol.io', 'www.stackportfol.io', 'stackportfol.io/']
 
 
 # Application definition
@@ -75,8 +75,8 @@ ROOT_URLCONF = 'stackportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
+        'DIRS': [TEMPLATES_DIR],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
