dff7ce074f9b8b28aee85a168788923344939b00
Stephen Dawson <stephendawson@protonmail.com>
2023-11-13 14:36:29 +0000

    prevent form submit,display dropdown list on input

--- static/js/techSelector.js
+++ static/js/techSelector.js
@@ -22,6 +22,30 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  // prevent form submission when enter is pressed
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  // when a key is pressed in the techInput,
+  // display the dropdown - filter out any non-
+  // matching items
+  techInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
+    if (value.length > 0) {
+      techDropdown.classList.remove("hidden");
+    } else {
+      techDropdown.classList.add("hidden");
+    }
+    techListItems.filter((item) => {
+      item.textContent.toLowerCase().includes(value) &&
+      !selectedTech.includes(item.textContent.trim())
+        ? item.classList.remove("hidden")
+        : item.classList.add("hidden");
+    });
+    dropdownVisible = techInput.value.length > 0;
+  });
+
   submitButton.addEventListener("click", (e) => {
     e.preventDefault();
   });
