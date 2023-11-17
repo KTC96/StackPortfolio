a04ce50693d2b069842470e08676d62f3f3bb89d
Stephen Dawson <stephendawson@protonmail.com>
2023-11-17 14:24:29 +0000

    update the job post model

--- job_post/models.py
+++ job_post/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.urls import reverse
-from django.utils.text import slugify
 from technology.models import Tech
 
 
-class Project(models.Model):
+class JobPost(models.Model):
     """
-    The project model. Projects have a one to many relationship
-    with the user model. A user can have many projects, but
-    a project can only have one user (for now).
+    The Job Post model. Job posts have a one to many relationship
+    with the user model. A user can have many job posts, but
+    a job post can only have one user. Job posts are created by
+    a recruiter user and viewed by anyone.
     """
     user = models.ForeignKey(
         "custom_account.CustomUser",
@@ -16,24 +16,27 @@ class Project(models.Model):
         related_name="job_posts")
     technologies = models.ManyToManyField(
         Tech, blank=True, related_name="job_posts")
-    name = models.CharField(max_length=100, blank=False, null=True)
+    name = models.CharField(max_length=100, blank=False,
+                            null=True)  # Job title
     active = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
     view_count = models.IntegerField(default=0)
-    slug = models.SlugField(blank=True, null=True)
+    company = models.CharField(max_length=80, blank=True, null=True)
+    salary_from = models.IntegerField(blank=True, null=True)
+    salary_to = models.IntegerField(blank=True, null=True)
+    salary_currency = models.CharField(max_length=3, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True,  null=True)
     date_updated = models.DateTimeField(auto_now=True,  null=True)
 
     class Meta:
         """
-        Meta class for the project model.
+        Meta class for the job post model.
         """
         ordering = ["-date_created"]
-        unique_together = ["user", "name"]
 
     def __str__(self):
         """
-        Returns the project name as a string.
+        Returns the job post name as a string.
         """
         return str(self.name)
 
@@ -48,22 +51,13 @@ class Project(models.Model):
         Returns the absolute url for a project.
         """
         return reverse(
-            "project:view_project",
+            "job_post:view_job_post",
             kwargs={
-                "slug": self.user.slug,
-                "project_slug": self.slug})
+                "slug": self.user.slug
+            })
 
     def save(self, *args, **kwargs):
         """
-        Override the save method to generate a slug if one doesn't already exist
-        and update the user's tech profile with approved technologies.
+        Saves the job post.
         """
-        if not self.slug:
-            self.slug = slugify(self.name)
-
-        super(Project, self).save(*args, **kwargs)
-
-        # This updates the tech on the tech user - requires a signal
-        # to run after many to many update
-        if self.user.tech_profile:
-            self.user.tech_profile.update_tech_with_approved()
+        super(JobPost, self).save(*args, **kwargs)
