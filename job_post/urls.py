b02bf70306022a40792fe58b787cf73a4db7725a
Stephen Dawson <stephendawson@protonmail.com>
2023-11-18 17:27:37 +0000

    add JobPostCreateView to job_post/urls.py

--- job_post/urls.py
+++ job_post/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import JobPostDetailView
+from .views import JobPostDetailView, JobPostCreateView
 
 app_name = 'job_post'
 
 urlpatterns = [
     path('<slug:slug>/job/<int:id>/',
          JobPostDetailView.as_view(), name='view_job_post'),
+    path('<slug:slug>/job/create', JobPostCreateView.as_view(),
+         name='create_job_post'),
 ]
