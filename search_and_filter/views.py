73522ee1affb3c92c3e823e9d40e739cd5b8dde7
Stephen Dawson <stephendawson@protonmail.com>
2023-11-22 19:16:09 +0000

    update view and template to handle search type

--- search_and_filter/views.py
+++ search_and_filter/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from project.models import Project
+from job_post.models import JobPost
+from custom_account.models import CustomUser
 from django.db.models import Q
 
 
@@ -9,19 +10,28 @@ class SearchResultsView(ListView):
     Handles the display of search results.
     """
     template_name = 'search_results_page.html'
-    model = Project
+    paginate_by = 9
 
     def get_queryset(self):
-        """
-        Returns the queryset of projects that match the search term.
-        """
         query = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', 'users')
 
-        if query:
-            object_list = Project.objects.filter(
+        if search_type == 'projects':
+            return Project.objects.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        elif search_type == 'job_posts':
+            return JobPost.objects.filter(
                 Q(name__icontains=query) | Q(description__icontains=query)
             )
         else:
-            object_list = Project.objects.all()
+            return CustomUser.objects.filter(
+                Q(first_name__icontains=query) | Q(
+                    last_name__icontains=query) | Q(bio__icontains=query)
+            )
 
-        return object_list
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_type = self.request.GET.get('type', 'users')
+        context['search_type'] = search_type
+        return context
