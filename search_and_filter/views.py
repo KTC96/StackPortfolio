a92f0fb31773479224a3f699f7015071a7134317
Stephen Dawson <stephendawson@protonmail.com>
2023-11-22 17:16:38 +0000

    create view to handle basic logic for search

--- search_and_filter/views.py
+++ search_and_filter/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from project.models import Project
+from django.db.models import Q
 
-# Create your views here.
+
+class SearchResultsView(ListView):
+    """
+    Handles the display of search results.
+    """
+    template_name = 'search_results_page.html'
+
+    model = Project
+
+    def get_queryset(self):
+        """
+        Returns the queryset of projects that match the search term.
+        """
+        query = "test"
+        object_list = Project.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+        return object_list
