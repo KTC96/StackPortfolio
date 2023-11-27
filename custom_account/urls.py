fc802b3501b8a24a9a8264e4478122462c6cc6b8
Stephen Dawson <stephendawson@protonmail.com>
2023-11-27 11:47:59 +0000

    move custom_account urls into own urls.py file

---
+++ custom_account/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from .views import (TechUserSignupView,
+                    RecruiterUserSignupView,
+                    UserProfileDetailView,
+                    UserProfileEditView,
+                    delete_user,
+                    AccountTypeView)
+
+app_name = 'custom_account'
+
+urlpatterns = [
+    path('accounts/signup/',
+         AccountTypeView.as_view(), name='account_type'),
+    path('accounts/signup/tech/',
+         TechUserSignupView.as_view(), name='tech_user_signup'),
+    path('accounts/signup/recruiter/',
+         RecruiterUserSignupView.as_view(), name='recruiter_user_signup'),
+    path('user/<slug:slug>/edit/',
+         UserProfileEditView.as_view(), name='profile_edit'),
+    path('user/<slug:slug>/delete/', delete_user, name='delete_user'),
+    path('user/<slug:slug>/', UserProfileDetailView.as_view(), name='user_profile'),
+]
--- stackportfolio/urls.py
+++ stackportfolio/urls.py
@@ -16,41 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from custom_account.views import (
-    TechUserSignupView,
-    RecruiterUserSignupView,
-    UserProfileDetailView,
-    IndexView,
-    UserProfileEditView,
-    delete_user,
-    AccountTypeView
-)
+from custom_account.views import IndexView
 from project.views import ProjectListView
 from job_post.views import JobPostListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/signup/',
-         AccountTypeView.as_view(), name='account_type'),
-    path('accounts/', include('allauth.urls')),
     path('', IndexView.as_view(), name='homepage'),
     path('contact/', TemplateView.as_view(template_name="contact.html"), name='contact'),
     path('careers/', TemplateView.as_view(template_name="careers.html"), name='careers'),
     path('about/', TemplateView.as_view(template_name="about_us.html"), name='about'),
-    path('accounts/signup/tech/',
-         TechUserSignupView.as_view(), name='tech_user_signup'),
-    path('accounts/signup/recruiter/',
-         RecruiterUserSignupView.as_view(), name='recruiter_user_signup'),
-    path('user/<slug:slug>/edit/',
-         UserProfileEditView.as_view(), name='profile_edit'),
-    path('user/<slug:slug>/delete/', delete_user, name='delete_user'),
-    path('user/<slug:slug>/', UserProfileDetailView.as_view(), name='user_profile'),
+    path('accounts/', include('allauth.urls')),
+    path('', include('custom_account.urls')),
     path('user/', include('project.urls')),
     path('user/', include('job_post.urls')),
     path('search/', include('search_and_filter.urls')),
     path('projects/', ProjectListView.as_view(), name='view_all_projects'),
     path('jobs/', JobPostListView.as_view(), name='view_all_job_posts'),
-
-
 ]
