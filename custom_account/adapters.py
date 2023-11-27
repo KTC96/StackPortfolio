5601b34acfa96b01415dc5493983407537d8cce8
Stephen Dawson <stephendawson@protonmail.com>
2023-11-27 19:31:44 +0000

    update social adapter code to add new tech profile

--- custom_account/adapters.py
+++ custom_account/adapters.py
@@ -17,19 +17,40 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
         try:
             user = User.objects.get(email=email)
             sociallogin.connect(request, user)
-            sociallogin.user = user
-            sociallogin.is_existing = True
         except User.DoesNotExist:
-            sociallogin.is_existing = False
+            pass
 
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
-        if not sociallogin.is_existing:
-            github_username = sociallogin.account.extra_data.get('login', '')
-            # Create a new tech user profile for the new user
-            TechUserProfile.objects.create(
+
+        github_name = sociallogin.account.extra_data.get('name', '')
+
+        if github_name:
+            try:
+                github_name = github_name.split(' ')
+                first_name = github_name[0]
+                last_name = github_name[-1]
+                user.first_name = first_name
+                user.last_name = last_name
+                user.work_title = ''
+                user.save()
+            except Exception as e:
+                print("Error updating user name:", e)
+        else:
+            user.first_name = "New"
+            user.last_name = "User"
+            user.work_title = ''
+            user.save()
+
+        github_username = sociallogin.account.extra_data.get('login', '')
+
+        try:
+            tech_profile = TechUserProfile.objects.create(
                 user=user,
                 github_username=github_username,
                 seeking_employment=False
             )
+        except Exception as e:
+            print("Error creating TechUserProfile:", e)
+
         return user
