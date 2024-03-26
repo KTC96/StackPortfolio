5375fd8a4b9ac97790584ebe3679495821d9ea59
Stephen Dawson <stephendawson@protonmail.com>
2024-03-26 21:20:09 +0000

    chore: add debug env variable

--- stackportfolio/settings.py
+++ stackportfolio/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG_MODE')
 
 ALLOWED_HOSTS = [
     'stackportfolio-1340b88e89e7.herokuapp.com',
